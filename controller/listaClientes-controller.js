76b1db23fafe28f99dca7417f5cbc844225d3ef9
rznd <grezende310@gmail.com>
2024-03-04 15:04:22 -0300

    refactoring async/await

--- controller/listaClientes-controller.js
+++ controller/listaClientes-controller.js
@@ -20,22 +20,26 @@ const criaNovaLinha = (nome, email, id) => {
 
 const tabela = document.querySelector("[data-tabela]")
 
-tabela.addEventListener("click", (evento) => {
+tabela.addEventListener("click", async (evento) => {
   let botaoDeletar = evento.target.className == "botao-simples botao-simples--excluir"
 
   if (botaoDeletar) {
     const linhaCliente = evento.target.closest("[data-id")
     let id = linhaCliente.dataset.id
-    clienteService.removeCliente(id)
-        .then(() => {
-            linhaCliente.remove()
-        })
+    await clienteService.removeCliente(id)
+    linhaCliente.remove()
   }
 })
 
-clienteService.listaClientes()
-    .then((data) => {
-        data.forEach((elemento) => {
-            tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
+const render = async () => {
+  const listaClientes = await clienteService.listaClientes()
+
+  listaClientes.forEach((elemento) => {
+              tabela.appendChild(criaNovaLinha(
+                elemento.nome, elemento.email, elemento.id
+                ))
   })
-})
+
+}
+
+render()
